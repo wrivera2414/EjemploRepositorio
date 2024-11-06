2b3f3337bc283b9a8a520cc04c81854f5af0c327
wrivera2414 <wencellrivera78@gmail.com>
2024-11-06 00:04:58 -0600

    agregando projecto de prueba
    agregado 1

---
+++ HolaMundoPrueba/src/main/java/com/mycompany/holamundoprueba/HolaMundoPrueba.java
@@ -0,0 +1,18 @@
+package com.mycompany.holamundoprueba;
+
+/**
+ * PROGRAMA DE PRUEBA
+ */
+import java.util.Scanner;
+
+public class HolaMundoPrueba {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in, "ISO-8859-1");
+        System.out.println("Mi Nombre Es Wencell Sánchez");
+        
+        
+        
+    }
+}
